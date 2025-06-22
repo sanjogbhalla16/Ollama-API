a85e4af8751c563f19826560194beba954ca9df0
sanjogbhalla16 <sanjog.bhalla@gmail.com>
2025-06-22 17:33:21 +0530

    making a verify the api key function

--- Ollama using FastAPI/main.py
+++ Ollama using FastAPI/main.py
@@ -1,13 +1,23 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Depends,HTTPException,Header
 import ollama
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
+API_KEY_CREDITS = {os.getenv("API_KEY"): 5}
+
+def verify_api_key(x_api_key: str = Header(None)):
+    credits = API_KEY_CREDITS.get(x_api_key,0)
+    if credits == 0:
+        raise HTTPException(status_code=401,details="Invalid API key, or no credits")
+    
+    return x_api_key
+
 app = FastAPI()
 
 @app.post("/generate")
-def generate(prompt: str):
+def generate(prompt: str, x_api_key: str = Depends(verify_api_key)):
+    API_KEY_CREDITS[x_api_key] -= 1
     response = ollama.chat(model="mistral", messages=[{"role": "user", "content": prompt }])
     return { "message": response["message"]["content"]}
